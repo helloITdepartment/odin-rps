f0d310144e07cc39214a159eb15d7f35c16b73a1
Jacques Benzakein <jackbenzakein@gmail.com>
2025-11-05 21:19:25 +0200

    Added functions to get computer and human moves

--- rps.js
+++ rps.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+//Options 0: Rock, 1: Paper, 2: Scissors 
+
+function getComputerChoice() {
+	return parseInt(Math.random() * 3);
+}
+
+function getHumanChoice() {
+	let humanChoice = prompt("Please enter your move");
+
+	if (humanChoice.toLowerCase().trim() === "rock") {
+		return 0;
+	} else if (humanChoice.toLowerCase().trim() === "paper") {
+		return 1;
+	} else if (humanChoice.toLowerCase().trim() === "scissors") {
+		return 2;
+	}
+}
