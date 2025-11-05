98767752cad51a3c15f671af57d1574f800abeb7
Jacques Benzakein <jackbenzakein@gmail.com>
2025-11-05 21:35:05 +0200

    Implemented code to play a single round

--- rps.js
+++ rps.js
@@ -1,4 +1,6 @@
 //Options 0: Rock, 1: Paper, 2: Scissors 
+let computerScore = 0;
+let humanScore = 0;
 
 function getComputerChoice() {
 	return parseInt(Math.random() * 3);
@@ -15,3 +17,38 @@ function getHumanChoice() {
 		return 2;
 	}
 }
+
+function playRound(computerChoice, humanChoice) {
+	if (humanChoice === computerChoice) {
+		console.log("It's a tie!");
+	} else if (humanChoice === 0) {
+		if (computerChoice === 2) {
+			console.log("Computer chose scissors, you win!");
+			humanScore++;
+		} else {
+			console.log("Computer chose paper, you loose");
+			computerScore++;
+		}
+	} else if (humanChoice === 1) {
+		if (computerChoice === 0) {
+			console.log("Computer chose rock, you win!");
+			humanScore++;
+		} else {
+			console.log("Computer chose scissors, you loose");
+			computerScore++;
+		}
+	} else if (humanChoice === 2) {
+		if (computerChoice === 1) {
+			console.log("Computer chose paper, you win!");
+			humanScore++;
+		} else {
+			console.log("Computer chose rock, you loose");
+			computerScore++;
+		}
+	}
+}
+
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice();
+
+playRound(computerChoice, humanChoice);
